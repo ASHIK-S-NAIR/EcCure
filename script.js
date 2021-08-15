0063fbb2c5fdf3637460f45660bf7570c74212f9
Ashik S Nair <aashiq5342@gmail.com>
2021-08-16 00:21:31 +0530

    final editing done

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
+window.addEventListener("load", function(){
+    // page-loader start
+    setTimeout(function(){
+        document.querySelector(".page-loader").style.display = "none";
+    },800);
+    // page-loader end
+
+    // animate on scroll start
+    AOS.init();
+    // animate on scroll end
+})
+
+
+
+// nav-toggler start
 const navToggle = document.querySelector(".nav-toggle");
 const navigationSection = document.querySelector(".navigation-section");
 
@@ -6,5 +22,25 @@ navToggle.addEventListener('click', Toggler);
 function Toggler(){
     navigationSection.classList.toggle('open');
     navToggle.classList.toggle('active');
-
 }
+// nav-toggler end
+
+
+// nav-select start
+document.addEventListener('click', (e)=>{
+    if(e.target.closest('.navigation-li')){
+        Select();
+    }
+})
+
+document.addEventListener('click', (e)=> {
+    if(e.target.closest('.navigation-social-a')){
+        Select();
+    }
+})
+
+function Select(){
+    navigationSection.classList.toggle('open');
+    navToggle.classList.toggle('active');
+}
+// nav-select end
